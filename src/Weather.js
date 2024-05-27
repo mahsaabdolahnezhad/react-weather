a9376785d625c46f2c64411cd05547be1ac1e0c1
mahsaabdolahnezhad <141028214+mahsaabdolahnezhad@users.noreply.github.com>
2024-05-27 14:39:15 +0330

    added api

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React , {useState} from "react";
 import "./Weather.css"
+import axios from "axios";
 
 export default function Weather(){
+    const [ready , setReady] = useState(false);
+    const [temprature , setTemprature] = useState("null");
+    function handleResponse(response){
+        console.log(response.temprature);
+        setTemprature(response.temprature.current);
+        setReady(true);
+    }
+ 
+    if (ready){
     return (<div className="Weather">
         <form>
             <div className="row">
@@ -30,7 +40,7 @@ export default function Weather(){
         <div className="row mt-3">
             <div className="col-6">
                 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy"/> 
-                <span className="temprature">6</span>
+                <span className="temprature">{temprature}</span>
                 <span className="unit">℃</span>
             </div>
             <div className="col-6">
@@ -49,5 +59,15 @@ export default function Weather(){
         </div>
         
         
-        </div>);
+        </div>)
+    }
+    else{
+        const apiKey="6a0e728f9903t4d8c372boc76730411b";
+        let city="Tehran";
+        let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+
+        return "Loading..."
+    }
+    ;
 }
