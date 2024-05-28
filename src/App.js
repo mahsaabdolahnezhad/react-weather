ce843da781abe58e04f02a281296593a9e02c004
mahsaabdolahnezhad <141028214+mahsaabdolahnezhad@users.noreply.github.com>
2024-05-28 13:14:02 +0330

    added api integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
     <footer>
       This project was coded by Mahsa Abdolahnezhad{" "}
       <a href="https://github.com/mahsaabdolahnezhad/react-weather" target="_blank" rel="noreferrer">open-sourced on github</a>
--- src/Weather.js
+++ src/Weather.js
@@ -2,16 +2,23 @@ import React , {useState} from "react";
 import "./Weather.css"
 import axios from "axios";
 
-export default function Weather(){
-    const [ready , setReady] = useState(false);
-    const [temprature , setTemprature] = useState("null");
+export default function Weather(props){
+    const [weatherData , setWeatherData] = useState({ready: false});
     function handleResponse(response){
-        console.log(response.temprature);
-        setTemprature(response.temprature.current);
-        setReady(true);
+        setWeatherData({
+        ready : true,
+        temprature: response.data.temperature.current,
+        humidity: response.data.temperature.humidity,
+        wind:response.data.wind.speed,
+        date:"Tuesday 12:00",
+        city:response.data.city,
+        description: response.data.condition.description,
+        iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        });
+       
     }
  
-    if (ready){
+    if (weatherData.ready){
     return (<div className="Weather">
         <form>
             <div className="row">
@@ -28,31 +35,28 @@ export default function Weather(){
             </div>
             </div>
         </form>
-        <h1>Tehran</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
             <li>
-             Sunday 1:00   
+            {weatherData.date}  
             </li>
-            <li>
-            Sunny
+            <li className="text-capitalize">
+            {weatherData.description}
             </li>
         </ul>
         <div className="row mt-3">
             <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy"/> 
-                <span className="temprature">{temprature}</span>
+                <img src={weatherData.iconUrl} alt={weatherData.description}/> 
+                <span className="temprature">{Math.round(weatherData.temprature)}</span>
                 <span className="unit">℃</span>
             </div>
             <div className="col-6">
                 <ul>
                     <li>
-                        Precipitation: 15%
+                        Humidity: {weatherData.humidity}%
                     </li>
                     <li>
-                        Humidity: 72%
-                    </li>
-                    <li>
-                        Wind: 13 km/h
+                        Wind: {weatherData.wind} km/h
                     </li>
                 </ul>
             </div>
@@ -63,11 +67,9 @@ export default function Weather(){
     }
     else{
         const apiKey="6a0e728f9903t4d8c372boc76730411b";
-        let city="Tehran";
-        let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
 
         return "Loading..."
-    }
-    ;
+    };
 }
